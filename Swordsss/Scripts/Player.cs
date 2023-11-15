b97f7047e218e3f81f2167e5d60acd8dab29926f
Mountain Cat <tomasz.banas123@gmail.com>
2023-11-15 18:23:43 +0100

    made a simple character controller

---
+++ Swordsss/Scripts/Player.cs
@@ -0,0 +1,51 @@
+using Godot;
+using System;
+
+public partial class Player : Node2D
+{
+    [Export] public float Speed { get; set; } = 200f;
+    [Export] public Texture2D Texture2D { get; set; } = null;
+    
+    private PlayerInput _playerInput;
+    private AnimatedSprite2D _animatedSprite2D;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        _playerInput = PlayerInput.Instance ?? throw new Exception("PlayerInput is not initialized");
+        
+        _playerInput.PlayerMoved += OnPlayerMoved; 
+        _playerInput.PlayerAttacked += OnPlayerAttacked;
+        
+                
+        _animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
+
+    private void OnPlayerAttacked()
+    {
+        var sprite = new Sprite2D();
+        AddChild(sprite);
+
+        sprite.Texture = Texture2D;
+        
+        GD.Print("XD");
+
+        var random = new RandomNumberGenerator();
+        
+        sprite.Position = new Vector2(500 * random.RandfRange(-1, 1), 500 * random.RandfRange(-1, 1 ));
+    }
+
+    private void OnPlayerMoved(float x, float y, double delta)
+    {
+        float multiplier = (float) delta * Speed;
+        
+        MoveLocalX(x * multiplier);
+        MoveLocalY(y * multiplier);
+        
+        if(x > 0)
+            _animatedSprite2D.FlipH = false;
+        if(x < 0)
+            _animatedSprite2D.FlipH = true;
+    }
+}
---
+++ Swordsss/Scripts/PlayerActions.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public static class PlayerActions 
+{
+    public const string Attack = "attack";
+    
+    public const string MoveLeft = "move_left";
+    public const string MoveRight = "move_right";
+    public const string MoveUp = "move_up";
+    public const string MoveDown = "move_down";
+}
